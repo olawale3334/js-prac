03633d5e716a0fd4b471677d48088032cfe8dd87
olawale3334 <olawale3334@gmail.com>
2025-04-07 00:48:35 +0100

    new added

--- ass.js
+++ ass.js
@@ -115,84 +115,151 @@
 
 // assignment task 2
 
-let moviesDb =[
-  {
-    title:"Archemy of soul",
-    id : 1,
-    Genre: "action",
-    year: 2022,
-   watched: 'true'
+// let moviesDb =[
+//   {
+//     title:"Archemy of soul",
+//     id : 1,
+//     Genre: "action",
+//     year: 2026,
+//    watched: 'true'
     
-  },
-  {
-    title:"taxi driver",
-    id : 2,
-    Genre: "action",
-    year: 2022,
-    watched: 'false'
+//   },
+//   {
+//     title:"taxi driver",
+//     id : 2,
+//     Genre: "action",
+//     year: 2022,
+//     watched: 'false'
 
     
-  },
-  {
-    title:"Archalugo",
-    id : 3,
-    Genre: "love",
-    year: 2025,
-    watched: 'false'
-  },
-  {
-    title:"all of us are dead",
-    id : 4,
-    Genre: "horror",
-    year: 2023,
-   watched: 'true'
-  },
-  {
-    title:"uncanny counter",
-    id : 5,
-    Genre: "action",
-    year: 2022,
-    watched: 'false'
-  },
-]
-let watch =[]
-let unwatched =[]
- 
-
-function addMovie( title,id, Genre,year,watched){
+//   },
+//   {
+//     title:"Archalugo",
+//     id : 3,
+//     Genre: "love",
+//     year: 2025,
+//     watched: 'false'
+//   },
+//   {
+//     title:"all of us are dead",
+//     id : 4,
+//     Genre: "horror",
+//     year: 2023,
+//    watched: 'true'
+//   },
+//   {
+//     title:"uncanny counter",
+//     id : 5,
+//     Genre: "action",
+//     year: 2022,
+//     watched: 'false'
+//   },
+// ]
+// let watch =[]
+// let unwatched =[]
+     
+// function addMovie( title,id, Genre,year,watched){
     
-   let movie = {
-     title: title,
-     id: id,
-     Genre:Genre,
-     year: year,
-     watched: watched
-}
-  moviesDb.push(movie)
-  console.log("moive watchlist after adding a new moives", moviesDb)
-}
-addMovie("Night agent",5,"action",2022, 'true')
-addMovie("Vicenzo casanova",6,"action",2021, 'true')
+//    let movie = {
+//      title: title,
+//      id: id,
+//      Genre:Genre,
+//      year: year,
+//      watched: watched
+// }
+//   moviesDb.push(movie)
+// }
+// addMovie("Night agent",6,"action",2022, 'true')
+// addMovie("Vicenzo casanova",7,"action",2021, 'true')
+// console.log("moive watchlist after adding a new moives",moviesDb )
 
-function markASWuw(mark){
-     for ( let movie in mark) {
-       if (mark[movie].watched === 'true'){
-         watch.push(mark[movie])
-       } else if (mark[movie].watched === 'false'){
-          unwatched.push(mark[movie])
-      }
-     }  console.log("result after mark as watched", watch)
-     console.log("result after mark as unwatched", unwatched)
-}
-  markASWuw(moviesDb)
+// function markASWuw(mark){
+//      for ( let movie in mark) {
+//        if (mark[movie].watched === 'true'){
+//          watch.push(mark[movie])
+//        } else if (mark[movie].watched === 'false'){
+//           unwatched.push(mark[movie])
+//       }
+//      }  console.log("result after mark as watched", watch)
+//      console.log("result after mark as unwatched", unwatched)
+// }
+//   markASWuw(moviesDb)
 
-  moviesDb.forEach(movie => {
-    console.log( `watched:${movie.watched} `)
-  });
-          function deleteM() {
-            moviesDb.pop()
-            console.log( "movie list after deleting one",moviesDb)
+//   moviesDb.forEach(movie => {
+//     console.log( `watched:${movie.watched} `)
+//   });
+//           function deleteM() {
+//             moviesDb.pop()
+//             console.log( "movie list after deleting one",moviesDb)
             
-          }
-          deleteM()
-          
+//           }
+//           deleteM()
+//        moviesDb.sort((first,second) => first.year-second.year )
+//        console.log(" watchlist after sorted by year",moviesDb)
+
+
+        // task 1
+         let todosApps = [
+          {
+            title:"pray",
+            id : 1,
+           completed: 'true'
+            
+          },
+          {
+            title:"pray",
+            id : 2,
+           completed: 'true'
+            
+          },
+          {
+            title:"wash",
+            id : 3,
+           completed: 'false'
+            
+          },
+          {
+            title:"go to gym",
+            id : 4,
+           completed: 'false'
+            
+          },
+         
+          ]
+          console.log(todosApps)
+            id = todosApps.length +1
+         function addTask ( title,completed){
+             let newadded = {
+              title:title,
+              id:id,
+              completed:completed
+             }  
+             todosApps.push(newadded)
+            }  
+            addTask("coding","true")
+            console.log("todos after adding a new todos",todosApps)
+            
+
+
+            let done=[]
+            let undone=[]
+
+            function markASWuw(mark){
+              for ( let hello in mark){
+                if ( mark[hello].completed === 'true'){
+                  done.push(mark[hello])
+                } else if(mark[hello].completed === 'false'){
+                  undone.push(mark[hello])
+                }
+              }
+            }
+            markASWuw(todosApps)
+            console.log("after mark as done",done)
+            console.log("after mark as undone",undone)
+
+
+            function deleteTask() {
+              todosApps = todosApps.filter((todosApps, i) => todosApps.id !== 4);
+              console.log(todosApps)
+            }
+            deleteTask()
