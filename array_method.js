68e8df89a85582e6768d7db56e3f449f677239f2
olawale3334 <olawale3334@gmail.com>
2025-04-09 02:31:08 +0100

    new added

--- array_method.js
+++ array_method.js
@@ -1,5 +1,5 @@
 // index of is use to check if an array contain a value. if it does it return the value but it not it return -1
-  let supers = ["superman","batman", "flash","aquaman",]
+  //let supers = ["superman","batman", "flash","aquaman",]
 // console.log(supers.includes("flash")) it shows true but if it doesnt exit in our array it shows  false 
 
  //console.log(supers.indexOf('flash'))
@@ -20,12 +20,37 @@
  //supers.sort();
  //console.log(supers);
  
-   let fruits =[ "apple","orange","banna"]
+   //let fruits =[ "apple","orange","banna"]
 //     fruits.unshift("cherry","pinapple")
 //    console.log(fruits)
 //    fruits.push( "plum","grape")
 //    console.log(fruits)
-   fruits.splice(1,1, "lemon")
-   console.log(fruits)
-    fruits.sort()
-    console.log(fruits)
+//    fruits.splice(1,1, "lemon")
+//    console.log(fruits)
+//     fruits.sort()
+//     console.log(fruits)
+
+
+// array iterator methods
+let supers=[ 'superman', 'batman', 'flash', 'aquaman',
+'thor', 'spiderman','wolveline','cyclos','professor x']
+//  supers.forEach(
+//     function(value,index,array){
+//         console.log(value)
+//     }
+//  );
+
+//map 
+// function uperCase(value){
+//    return value.toUpperCase();
+// }
+// let supersCap = supers.map(uperCase)
+// console.log(supersCap);
+ let num = [1,2,3]
+function double(x){
+    return  x * 3;
+ }
+ let doubleN = num.map(double)
+ console.log(doubleN)
+ console.log(num);
+ 
